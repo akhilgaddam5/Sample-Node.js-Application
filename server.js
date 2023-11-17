fb516c034b6e41f5ccf64b7d4cd042c1977d7d1b
akhilgaddam5 <144714253+akhilgaddam5@users.noreply.github.com>
2023-11-16 23:26:42 -0600

    server.js

--- server.js
+++ server.js
@@ -1,48 +1,74 @@
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    cookieSession = require('cookie-session'),
-    serveStatic = require('serve-static'),
-    expressValidator = require('express-validator'),
-    passport = require('passport'),
-    crypto = require('crypto'),
-    flash = require('connect-flash'),
-    mongoose = require('mongoose'),
-    morgan       = require('morgan');
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
-var handlebars = require('express-handlebars').create({
-    layoutsDir: "views/layouts",
-    partialsDir: "views/partials",
-    defaultLayout: 'main'
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// File upload endpoint
+app.post('/upload', upload.single('file'), (req, res) => {
+  const file = req.file;
+  const fileName = file.originalname;
+
+  // Check if the file already exists
+  if (fs.existsSync(path.join(upload.dest, fileName))) {
+    // If the file exists and the "override" query parameter is set, delete the existing file
+    if (req.query.override === 'true') {
+      fs.unlinkSync(path.join(upload.dest, fileName));
+    } else {
+      return res.status(409).send('File already exists.');
+    }
+  }
+
+  // Move the uploaded file to the "uploads" directory
+  fs.renameSync(file.path, path.join(upload.dest, fileName));
+  res.send('File uploaded successfully.');
 });
 
-//Intialise express framework
-var app = express();
+// File download endpoint
+app.get('/download/:filename', (req, res) => {
+  const fileName = req.params.filename;
+  const filePath = path.join(upload.dest, fileName);
 
-app.use(morgan('dev')); // log every request to the console
-mongoose.connect('mongodb://127.0.0.1:27017/samplenodejsapp')
+  // Check if the file exists
+  if (fs.existsSync(filePath)) {
+    res.download(filePath);
+  } else {
+    res.status(404).send('File not found.');
+  }
+});
 
-require('./src/middleware/passport')(passport);
+// File listing endpoint
+app.get('/files', (req, res) => {
+  fs.readdir(upload.dest, (err, files) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error retrieving files.');
+    }
+    res.json(files);
+  });
+});
 
-app.use(cookieParser('SampleNode.jsApp')); //read cookies
-app.use(cookieSession({
-    keys: ['key1', 'key2']
-}));
+// File deletion endpoint
+app.delete('/delete/:filename', (req, res) => {
+  const fileName = req.params.filename;
+  const filePath = path.join(upload.dest, fileName);
 
-app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.set('views', __dirname + '/views'); // set template directory
-app.set('view engine', 'handlebars'); //set view engine to handlebars
-app.engine('handlebars', handlebars.engine);
-app.use(expressValidator());
-app.use(passport.initialize());
-app.use(passport.session()); //persistant login sessions
-app.use(serveStatic('./public')); //serve static files from public directory
-app.use(flash()); //used to display flash messages from sessions
+  // Check if the file exists
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath);
+    res.send('File deleted successfully.');
+  } else {
+    res.status(404).send('File not found.');
+  }
+});
 
-require('./routes')(app, passport); // include routes
-app.listen(process.env.PORT || 3000);
-
-console.log('Listening on port 3000');
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
